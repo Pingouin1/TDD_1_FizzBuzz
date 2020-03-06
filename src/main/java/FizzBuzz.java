80536504321b6436bf702f5c00718b84d981ba2b
Tanguy Bouchet <tanguy.bouchet@gmail.com>
2020-03-06 15:10:51 +0100

    red

---
+++ src/main/java/FizzBuzz.java
@@ -0,0 +1,6 @@
+public class FizzBuzz {
+
+    public String fizzBuzz(int n) {
+        return "";
+    }
+}
---
+++ src/test/java/FizzBuzzTest.java
@@ -0,0 +1,35 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FizzBuzzTest {
+
+    private final static FizzBuzz fizzBuzz = new FizzBuzz();
+
+    @Test
+    public final void fizzbuzz_should_return_empty_string_if_not_dividable_by_three_or_five() {
+        int n = 1;
+
+        assertEquals("should return empty string if not dividable by three or five", "", fizzBuzz.fizzBuzz(n));
+    }
+
+    @Test
+    public final void fizzbuzz_should_return_fizz_if_dividable_by_three() {
+        int n = 3;
+
+        assertEquals("should return empty string if not dividable by three or five", "Fizz", fizzBuzz.fizzBuzz(n));
+    }
+
+    @Test
+    public final void fizzbuzz_should_return_buzz_if_dividable_by_five() {
+        int n = 5;
+
+        assertEquals("should return empty string if not dividable by three or five", "Buzz", fizzBuzz.fizzBuzz(n));
+    }
+    @Test
+    public final void fizzbuzz_should_return_buzz_if_dividable_by_three_and_five() {
+        int n = 15;
+
+        assertEquals("should return empty string if not dividable by three or five", "FizzBuzz", fizzBuzz.fizzBuzz(n));
+    }
+}

9b83b64b47ecca303c318e03c1e046fb2ce4f712
Tanguy Bouchet <tanguy.bouchet@gmail.com>
2020-03-06 15:15:59 +0100

    green

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,6 +1,15 @@
 public class FizzBuzz {
 
     public String fizzBuzz(int n) {
-        return "";
+        String result = "";
+
+        if (n % 3 == 0) {
+            result += "Fizz";
+        }
+
+        if (n%5 == 0) {
+            result += "Buzz";
+        }
+        return result;
     }
 }

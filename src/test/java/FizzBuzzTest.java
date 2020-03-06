eb1a36478d2e30c774c3886ffa228ad05193ddb3
Tanguy Bouchet <tanguy.bouchet@gmail.com>
2020-03-06 15:20:13 +0100

    red with new requirements

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -7,29 +7,44 @@ public class FizzBuzzTest {
     private final static FizzBuzz fizzBuzz = new FizzBuzz();
 
     @Test
-    public final void fizzbuzz_should_return_empty_string_if_not_dividable_by_three_or_five() {
+    public final void should_return_empty_string_if_not_dividable_by_three_or_five() {
         int n = 1;
 
         assertEquals("should return empty string if not dividable by three or five", "", fizzBuzz.fizzBuzz(n));
     }
 
     @Test
-    public final void fizzbuzz_should_return_fizz_if_dividable_by_three() {
+    public final void should_return_fizz_if_dividable_by_three() {
         int n = 3;
 
         assertEquals("should return empty string if not dividable by three or five", "Fizz", fizzBuzz.fizzBuzz(n));
     }
 
     @Test
-    public final void fizzbuzz_should_return_buzz_if_dividable_by_five() {
+    public final void should_return_buzz_if_dividable_by_five() {
         int n = 5;
 
         assertEquals("should return empty string if not dividable by three or five", "Buzz", fizzBuzz.fizzBuzz(n));
     }
+
     @Test
-    public final void fizzbuzz_should_return_buzz_if_dividable_by_three_and_five() {
+    public final void should_return_buzz_if_dividable_by_three_and_five() {
         int n = 15;
 
         assertEquals("should return empty string if not dividable by three or five", "FizzBuzz", fizzBuzz.fizzBuzz(n));
+    }
+
+    @Test
+    public final void should_return_buzz_if_finishes_by_three() {
+        int n = 13;
+
+        assertEquals("should return empty string if not dividable by three or five", "Fizz", fizzBuzz.fizzBuzz(n));
+    }
+
+    @Test
+    public final void should_return_buzz_if_finishes_by_five() {
+        int n = 25;
+
+        assertEquals("should return empty string if not dividable by three or five", "Buzz", fizzBuzz.fizzBuzz(n));
     }
 }
